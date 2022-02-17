f28d1c7c446a6343d1b99c18c3590fb0ff4bfc77
imtiazahammed1 <imtiaz2015160042@gmail.com>
2022-02-17 21:21:25 +0600

    javascript-all-added

--- expenditure.js
+++ expenditure.js
@@ -1,29 +1,60 @@
+// functioned number 1
+//  Expenses and Balance Function
+
 function updatevalue() {
     const incomeInput = document.getElementById('income-input');
     const incomeValue = incomeInput.value;
     const product = parseFloat(incomeValue);
-    console.log(product);
     const foodieInput = document.getElementById('foodie-input');
     const foodieValue = foodieInput.value;
     const product1 = parseFloat(foodieValue);
-    console.log(product1);
+    foodieInput.value = '';
     const horentInput = document.getElementById('horent-input');
     const horentValue = horentInput.value;
     const product2 = parseFloat(horentValue);
-    console.log(product2);
+    horentInput.value = '';
     const clothsInput = document.getElementById('cloths-input');
     const clothsValue = clothsInput.value;
     const product3 = parseFloat(clothsValue);
-    console.log(product3);
+    clothsInput.value = '';
     totalExpenses = product1 + product2 + product3;
-    console.log(totalExpenses);
+    const totalInput = document.getElementById('total-expenses');
+    const totalInputText = totalInput.innerText;
+    totalInput.innerText = totalExpenses;
     balance = product - totalExpenses;
-    console.log(balance);
+    const balanceInput = document.getElementById('balance-remain');
+    const balanceInputText = balanceInput.innerText;
+    balanceInput.innerText = balance;
+    return balance;
+
 
 }
 
+// After Saving Remaining Balance
+// functioned number 2
+function saveupdatevalue() {
+    const saveInput = document.getElementById('save-input');
+    const saveValue = saveInput.value;
+    const product4 = parseFloat(saveValue);
+    saveInput.value = '';
+    const incomeInput = document.getElementById('income-input');
+    const incomeValue = incomeInput.value;
+    const product = parseFloat(incomeValue);
+    incomeInput.value = '';
+    const savingAmount = document.getElementById('saving-amount');
+    savingAmount.innerText = (product * product4) / 100;
+    const savingAmountText = savingAmount.innerText;
+    const remainingBalance = document.getElementById('remaining-balance');
+    const remainingBalanceText = remainingBalance.innerText;
+    remainingBalance.innerText = balance - savingAmountText;
 
+}
+
+//  Function calling number 1
 document.getElementById('calcaulate-total').addEventListener('click', function () {
     updatevalue();
-
-})
+});
+//  Function calling number 2
+document.getElementById('save-total').addEventListener('click', function () {
+    saveupdatevalue();
+});
